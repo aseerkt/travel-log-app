30fca6073f464ebb28e74f1be6147f1cf8ddbdff
Aseer KT <aseermiqdad@gmail.com>
2021-05-04 20:09:19 +0530

    check: hopefull fix transpile issue of mapbox

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 // @ts-ignore
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
 // @ts-ignore
-import MapboxWorker from 'mapbox-gl/dist/mapbox-gl-csp-worker';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
 import './index.css';
 import App from './App';
 
