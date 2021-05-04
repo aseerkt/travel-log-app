0dfcfe96bc1c1ce19bb52b9757b74db1718b4f88
Aseer KT <aseermiqdad@gmail.com>
2021-05-04 20:21:14 +0530

    trial: mapbox officail fix for transpiling v2

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import mapboxgl from 'mapbox-gl';
-
+// @ts-ignore
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
+// @ts-ignore
 // eslint-disable-next-line import/no-webpack-loader-syntax
-(mapboxgl as any).workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
+
+mapboxgl.workerClass = MapboxWorker;
 
 ReactDOM.render(
   <React.StrictMode>
