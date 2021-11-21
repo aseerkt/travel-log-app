e7af97ea89596a870691053d2ec278a1efc8bb54
Aseer KT <aseermiqdad@gmail.com>
2021-11-21 21:51:15 +0530

    fix: import module error

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,11 +6,10 @@ import mapboxgl from 'mapbox-gl';
 // @ts-ignore
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
+import App from './App';
 
 // @ts-ignore
 mapboxgl.workerClass = MapboxWorker;
-
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
