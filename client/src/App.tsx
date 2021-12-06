acda077b5b3dcf785d99809d46a4397c311e3c1d
Aseer KT <aseermiqdad@gmail.com>
2021-12-06 12:20:20 +0530

    chore: code splitting for routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,21 @@
 import axios from 'axios';
+import { Suspense, lazy } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { AuthProvider } from './context/AuthContext';
 import PrivateRoute from './PrivateRoute';
 import Layout from './components/Layout';
-// Page Components
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import MapPage from './pages/MapPage';
-import LogsPage from './pages/LogsPage';
-import LogPage from './pages/LogPage';
-import ProfilePage from './pages/ProfilePage';
-import AddLogPage from './pages/AddLogPage';
 import { API_URL } from './config';
+import Loader from './components/Loader';
+// Page Components
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+const RegisterPage = lazy(() => import('./pages/RegisterPage'));
+const MapPage = lazy(() => import('./pages/MapPage'));
+const LogsPage = lazy(() => import('./pages/LogsPage'));
+const LogPage = lazy(() => import('./pages/LogPage'));
+const ProfilePage = lazy(() => import('./pages/ProfilePage'));
+const AddLogPage = lazy(() => import('./pages/AddLogPage'));
 
 axios.defaults.baseURL = `${API_URL}/api`;
 axios.defaults.headers = {
@@ -25,28 +27,30 @@ const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AuthProvider>
-          <Layout>
-            <Switch>
-              <Route exact path='/' component={LogsPage} />
-              <Route exact path='/logs/:id' component={LogPage} />
-              <Route exact path='/login' component={LoginPage} />
-              <PrivateRoute exact path='/my-logs-map' component={MapPage} />
-              <PrivateRoute
-                exact
-                path='/add-log?lat=:latitude&long=:longitude'
-                component={AddLogPage}
-              />
-              <Route exact path='/u/:userId' component={ProfilePage} />
-              <Route exact path='/register' component={RegisterPage} />
-            </Switch>
-            <ReactQueryDevtools initialIsOpen />
-          </Layout>
-        </AuthProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <Suspense fallback={<Loader />}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AuthProvider>
+            <Layout>
+              <Switch>
+                <Route exact path='/' component={LogsPage} />
+                <Route exact path='/logs/:id' component={LogPage} />
+                <Route exact path='/login' component={LoginPage} />
+                <PrivateRoute exact path='/my-logs-map' component={MapPage} />
+                <PrivateRoute
+                  exact
+                  path='/add-log?lat=:latitude&long=:longitude'
+                  component={AddLogPage}
+                />
+                <Route exact path='/u/:userId' component={ProfilePage} />
+                <Route exact path='/register' component={RegisterPage} />
+              </Switch>
+              <ReactQueryDevtools initialIsOpen />
+            </Layout>
+          </AuthProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Suspense>
   );
 };
 
